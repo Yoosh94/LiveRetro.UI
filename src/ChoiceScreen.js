4a04418b90d722d4b928f05a0deeb9636777e291
Ayush Kumar <kmar.ayush@gmail.com>
2019-09-05 17:42:09 +1000

    Add validation for display name room name

--- src/ChoiceScreen.js
+++ src/ChoiceScreen.js
@@ -4,22 +4,36 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class ChoiceScreen extends React.Component{
     constructor(props){
         super(props);
-        this.state={};
+        this.state={roomName:"",displayName:""};
     }
 
     handleRoomNameUpdate = (event) =>{
         this.setState({roomName:event.target.value});
     }
 
+    handleDisplayNameUpdate = (event) =>{
+        this.setState({displayName:event.target.value});
+    }
+
     handleJoinRoom = (e) =>{
+        this.validateDetails();
         this.props.handleJoinRoom(this.state.roomName);
+    }
+
+    validateDetails =() =>{   
+        return this.state.displayName.length > 0 && this.state.roomName.length > 0;
     }
     
     render(){
         return(
             <div>
+                <label>
+                    Display Name: 
+                </label>
+                <input type="text" name="displayName" onChange={this.handleDisplayNameUpdate}/>
+                <br/>
                 <button type='button'onClick={this.handleJoinRoom}>Join Room</button>
-                <input type="text" name="name" onChange={this.handleRoomNameUpdate}/>
+                <input type="text" name="roomCode" onChange={this.handleRoomNameUpdate}/>
                 <Link to='/create' onClick={this.props.handleCreateRoom}>Create Room</Link>
             </div>
         )
