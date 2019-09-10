e31fc8cd5fd656e5612da0128c3ae01e605e6319
Ayush Kumar <kmar.ayush@gmail.com>
2019-09-11 08:49:04 +1000

    Added button to room to add sticky note

--- src/Room.js
+++ src/Room.js
@@ -6,12 +6,13 @@ class Room extends React.Component{
         this.state={}
     }
 
+
     render(){
         return(
-            <div><h3>Room Code : {this.props.roomCode}</h3>
-            {this.props.adminMode ? null : <div><h2>WAITING FOR ACCESS</h2></div>}
-            </div>
-            
+            <div>
+                <h3>Room Code : {this.props.roomCode}</h3>
+                <button>+</button>
+            </div>           
         )
     }
 }
