46ee8bb19967adf02e592c8e22b11bfa4fb9f149
Ayush Kumar <kmar.ayush@gmail.com>
2019-09-12 08:08:48 +1000

    When button is clicked a note object appears

---
+++ src/Note.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+export default class Note extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+    render(){
+        return(
+                <div style={{width:'100px',height:'100px',background:'#f00'}}>
+
+            </div>
+        )
+    }
+            
+        
+    
+}
--- src/Room.js
+++ src/Room.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-
+import Note from './Note'
 class Room extends React.Component{
     constructor(props){
         super(props);
-        this.state={}
+        this.state={
+            notes:[]
+        }
     }
 
+
+    CreateNote = () =>{
+        this.setState({
+            notes:[
+                ...this.state.notes,
+                <Note key={this.state.notes.length}/>
+            ]
+        })
+    };
 
     render(){
         return(
             <div>
                 <h3>Room Code : {this.props.roomCode}</h3>
-                <button>+</button>
+                <button onClick={this.CreateNote}>+</button>
+                <div>
+                    {this.state.notes.map(note=> note)}
+                </div>
             </div>           
         )
     }
